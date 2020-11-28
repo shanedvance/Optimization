7d83d15ab1600719fdd48d769fa5963049f8be2c
shanevance <43920728+shanevance@users.noreply.github.com>
2020-11-27 23:27:03 -0800

    fix formatting in shell file

--- src/NEH/Run.sh
+++ src/NEH/Run.sh
@@ -10,22 +10,22 @@ set -e
 # function for the output of the NEH executable
 function output()
 {
-	# get our files from the specified directory and sort them properly
-	FILE=$(find $1 -type f -name '*.txt' -exec ls -v {} +)
-	# iterate and execute each file for the NEH executable
-	while read -r file; do
-		./NEH $file | tr '\n' ','
-		printf '\n'
-	done <<< "$FILE"
+  # get our files from the specified directory and sort them properly
+  FILE=$(find $1 -type f -name '*.txt' -exec ls -v {} +)
+  # iterate and execute each file for the NEH executable
+    while read -r file; do
+    ./NEH $file | tr '\n' ','
+    printf '\n'
+  done <<< "$FILE"
 }
 
 ARGS=2
 
 # do some error handling
 if [ $# -ne "$ARGS" ]; then
-	echo "Incorrect parameter count. Need 2 parameter values."
-	echo "Example: ./Run.sh Taillard_Series Results.csv"
-	exit 1
+  echo "Incorrect parameter count. Need 2 parameter values."
+  echo "Example: ./Run.sh Taillard_Series Results.csv"
+  exit 1
 fi
 
 # compute our results
